feaebb88f167efaaab97c8310526b7ee2fe44220
Yerassyl Maikhanov <maihanov96@gmail.com>
2019-06-06 04:38:23 +0600

    add carousel to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  images = [1, 2, 3].map(() => `https://picsum.photos/1600/450?random&t=${Math.random()}`);
+  constructor() {
+  }
 
   ngOnInit() {
   }
