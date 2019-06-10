31eb65ec8630b8a3222232c291084735bbe46bf4
MaksimBezmen <mamant@yandex.ru>
2019-06-10 22:19:04 +0300

    Revert "sort"
    This reverts commit 3dc18345a940f1b2179620be4447e3ef612152a6.
    
    # Conflicts:
    #	src/main/java/com/github/yerasis/talkitstore/service/BookSort.java

--- src/main/java/com/github/yerasis/talkitstore/service/BookSort.java
+++ src/main/java/com/github/yerasis/talkitstore/service/BookSort.java
@@ -8,32 +8,32 @@ import java.util.List;
 @Service
 public class BookSort {
 
-    public List<Book> sortByCostLowest(List<Book> books) {
-        if (books.isEmpty()) {
-            throw new NullPointerException( "Can't sort empty List" );
-        }
-        int in;
-        for (int out = 0; out < books.size() - 1; out++) {
-            Book temp = books.get( out );
-            in = out;
-            while (in > 0 && books.get( in - 1 ).getCost() > temp.getCost()) {
-                books.set( in, books.get( in - 1 ) );
-                in--;
-            }
-            books.set( in, temp );
-        }
-        return books;
+  public List<Book> sortByCostLowest(List<Book> books) {
+    if (books.isEmpty()) {
+      throw new NullPointerException("Can't sort empty List");
     }
+    int in;
+    for (int out = 0; out < books.size() - 1; out++) {
+      Book temp = books.get(out);
+      in = out;
+      while (in > 0 && books.get(in - 1).getCost() > temp.getCost()) {
+        books.set(in, books.get(in - 1));
+        in--;
+      }
+      books.set(in, temp);
+    }
+    return books;
+  }
 
-    public List<Book> sortByCostAscending(List<Book> books) {//сортирует цену по возростанию
-        if (books.isEmpty()) {
-            throw new NullPointerException( "Can't sort empty List" );
-        }
-        Collections.sort( books, new Comparator<Book>() {
-            public int compare(Book one, Book other) {
-                return one.getCost().compareTo( other.getCost() );
-            }
-        }
+  public List<Book> sortByCostAscending (List <Book> books){//сортирует цену по возростанию
+    if (books.isEmpty()) {
+      throw new NullPointerException("Can't sort empty List");
     }
+    Collections.sort(books, new Comparator<Book>() {
+      public int compare(Book one, Book other) {
+        return one.getCost().compareTo( other.getCost() );
+      }
+    }
+  }
 
 }
